5fb2e56f539d778e4a51a12152ab6ce7c46467a3
xrajesh <xrajesh@gmail.com>
2021-03-12 09:17:58 -0500

    move test to e2e

--- pkg/config/kubeClient_test.go
+++ pkg/config/kubeClient_test.go
@@ -7,12 +7,6 @@ import (
 	"testing"
 )
 
-func TestGetKubeClient(t *testing.T) {
-	if GetKubeClient() == nil {
-		t.Fatal("GetKubeClient failed - Cannot get KubeClient")
-	}
-}
-
 func TestRESTClient(t *testing.T) {
 	if RESTClient(GetConfig()) == nil {
 		t.Fatal("RESTClient failed - Cannot get RESTClient")
