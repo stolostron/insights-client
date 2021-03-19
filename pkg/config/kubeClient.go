e1b4d82d725ed3052f015ed4b57222dcb7e5eae6
Zack Layne <zlayne@redhat.com>
2021-03-19 09:40:52 -0400

    re-add rest client

--- pkg/config/kubeClient.go
+++ pkg/config/kubeClient.go
@@ -6,12 +6,22 @@ import (
     "sync"
 	"github.com/golang/glog"
 
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
     "k8s.io/client-go/dynamic"
+	"sigs.k8s.io/wg-policy-prototypes/policy-report/api/v1alpha1"
 )
 
+var CRDGroup string = "wgpolicyk8s.io"
+var CRDVersion string = "v1alpha1"
+var SchemeGroupVersion = schema.GroupVersion{Group: CRDGroup, Version: CRDVersion}
+var restClient *rest.RESTClient
 var clientSet *kubernetes.Clientset
 var mutex sync.Mutex
 var dynamicClient dynamic.Interface
@@ -26,6 +36,37 @@ func GetConfig() *rest.Config {
 		clientConfig, _ = rest.InClusterConfig()
 	}
 	return clientConfig
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&v1alpha1.PolicyReport{},
+	)
+	meta_v1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
+
+func RESTClient(cfg *rest.Config) *rest.RESTClient {
+	if restClient != nil {
+		return restClient
+	}
+	scheme := runtime.NewScheme()
+	SchemeBuilder := runtime.NewSchemeBuilder(addKnownTypes)
+	if err := SchemeBuilder.AddToScheme(scheme); err != nil {
+		glog.Warningf("Cannot add scheme %v", err)
+		return nil
+	}
+	config := *cfg
+	config.GroupVersion = &SchemeGroupVersion
+	config.APIPath = "/apis"
+	config.ContentType = runtime.ContentTypeJSON
+	config.NegotiatedSerializer = serializer.NewCodecFactory(scheme)
+	restClient, err := rest.RESTClientFor(&config)
+	if err != nil {
+		glog.Warningf("Error creating RestClient %v", err)
+		return nil
+	}
+	return restClient
 }
 
 // Get the kubernetes dynamic client.
