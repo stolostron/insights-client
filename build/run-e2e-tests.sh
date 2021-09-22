6240fff76fb9e038af652df0a5718d5859df1ec2
xavier dharmaiyan <xavier@redhat.com>
2021-09-22 14:03:39 -0400

    fixed work dir
    Signed-off-by: xavier dharmaiyan <xavier@redhat.com>

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -106,6 +106,7 @@ delete_command_binaries(){
 
 initial_setup() {
     echo $WORKDIR
+	cd ${WORKDIR}
     echo "=====Deploying insights-client====="
 	$sed_command "s~{{ INSIGHTS_CLIENT_IMAGE }}~$IMAGE_NAME~g" ./test-data/e2e/insights-chart/templates/insights-deployment.yaml
 	$sed_command "s~{{ INSIGHTS_CLIENT_CCX_TOKEN }}~$INSIGHTS_CLIENT_CCX_TOKEN~g" ./test-data/e2e/insights-chart/templates/insights-deployment.yaml
