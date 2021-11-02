394d60cfa9149dc723f08d65b30850c726d52b2c
mqhackett <mahacket@redhat.com>
2021-11-02 11:43:28 -0400

    add test for GetConfig
    Signed-off-by: mqhackett <mahacket@redhat.com>

---
+++ pkg/config/kubeClient_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+
+package config
+
+import (
+
+    "reflect"
+	"testing"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
+)
+
+// should build config from flags if kube config is not empty
+func Test_GetConfig(t *testing.T) {
+
+	clientConfig := GetConfig()
+	fromFlags, _ := clientcmd.BuildConfigFromFlags("", Cfg.KubeConfig)
+    fromRest, _ := rest.InClusterConfig()
+
+	if Cfg.KubeConfig != "" && !reflect.DeepEqual(*clientConfig, *fromFlags) { // if config is not empty, then get config from flags
+		t.Errorf("Failed testing GetConfig()  Expected: %s  Got: %s", fromFlags, clientConfig)
+	}
+    if Cfg.KubeConfig == "" && !reflect.DeepEqual(*clientConfig, *fromRest) { // if the config is empty, get using rest client
+		t.Errorf("Failed testing GetConfig()  Expected: %s  Got: %s", fromRest, clientConfig)
+	}
+
+}
