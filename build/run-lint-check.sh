3724022037c3b9029959b6e74cf23dcd4a582a89
Sherin Varughese <shvarugh@redhat.com>
2022-07-19 13:59:13 -0400

    install lint
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- build/run-lint-check.sh
+++ build/run-lint-check.sh
@@ -4,6 +4,7 @@ GOLANGCI_LINT_CACHE=/tmp/golangci-cache
 GOOS=$(go env GOOS)
 GOPATH=$(go env GOPATH)
 export GOFLAGS=""
-DOWNLOAD_URL="https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_LINT_VERSION}/golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64.tar.gz"
-curl -sfL "${DOWNLOAD_URL}" | tar -C "${GOPATH}/bin" -zx --strip-components=1 "golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64/golangci-lint"
-$(GOLANGCI_LINT_CACHE=${GOLANGCI_LINT_CACHE} CGO_ENABLED=0 golangci-lint run)
+DOWNLOAD_URL="https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh"
+# curl -sfL "${DOWNLOAD_URL}" | tar -C "${GOPATH}/bin" -zx --strip-components=1 "golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64/golangci-lint"
+curl -sSfL "${DOWNLOAD_URL}" | sh -s -- -b "${GOPATH}/bin" v1.47.1
+$(GOLANGCI_LINT_CACHE=${GOLANGCI_LINT_CACHE} CGO_ENABLED=0 GOGC=25 golangci-lint run)
