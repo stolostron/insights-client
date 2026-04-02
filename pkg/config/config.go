7a6b4cc795f07e34082f66938cdd4a223d67ed53
Zack Layne <zlayne@redhat.com>
2026-04-02 16:36:19 -0400

    [CCXDEV-16275] Set default CCX_SERVER to public API (#618)
    * [CCXDEV-16275] Set default CCX_SERVER to public API
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    * fix doc url
    
    Signed-off-by: zlayne <zlayne@redhat.com>
    
    ---------
    
    Signed-off-by: zlayne <zlayne@redhat.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -14,7 +14,7 @@ import (
 const (
 	DEFAULT_SERVICE_PORT     = ":3030"
 	DEFAULT_HTTP_TIMEOUT     = 180000                                  // 3 minutes HTTP Timeout
-	DEFAULT_CCX_SERVER       = "http://localhost:8080/api/v1/clusters" // For local use only
+	DEFAULT_CCX_SERVER       = "https://console.redhat.com/api/insights-results-aggregator/v2"
 	DEFAULT_POLL_INTERVAL    = 30                                      // 30mins default polling interval cloud.redhat.com
 	DEFAULT_REQUEST_INTERVAL = 1                                       // 1 second Interval between 2 consecutive requests
 	DEFAULT_POD_NAMESPACE    = "kube-system"                           // Namespace of insights-client pod
